c98db7897d4eed7d10785f4cb54c172afdd83b70
Lemon-Juiced <tbrotheawesome@gmail.com>
2022-12-14 23:13:28 -0500

    Update Runedropper's Capabilities

--- src/main/java/lemon_juice/scalar_utils/block/custom/AbstractRunedropperBlock.java
+++ src/main/java/lemon_juice/scalar_utils/block/custom/AbstractRunedropperBlock.java
@@ -1,30 +1,28 @@
 package lemon_juice.scalar_utils.block.custom;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 
 public class AbstractRunedropperBlock extends Block {
-    private final int seconds;
+    private final int blocks;
 
-    public AbstractRunedropperBlock(Properties properties, int seconds) {
+    public AbstractRunedropperBlock(Properties properties, int blocks) {
         super(properties);
-        this.seconds = seconds;
+        this.blocks = blocks;
     }
 
+    //Stops Entities from taking fall damage on unless they "suppress the bounce"
     @Override
-    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
-        if(!level.isClientSide() && hand == InteractionHand.MAIN_HAND){
-            player.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, seconds * 20));
+    public void fallOn(Level level, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
+        if(entity.isSuppressingBounce()){
+            super.fallOn(level, state, pos, entity, fallDistance);
+        } else {
+            entity.causeFallDamage(fallDistance-blocks, 1.0F, DamageSource.FALL);
         }
-
-        return super.use(state, level, pos, player, hand, result);
     }
+
 }
