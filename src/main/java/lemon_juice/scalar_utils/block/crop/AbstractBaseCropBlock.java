ce01cb3891954ae60bcf4aa7d5d169514bd421c9
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-01-04 23:40:38 -0500

    Refactored name of AbstractBaseCrop to AbstractBaseCropBlock

--- src/main/java/lemon_juice/scalar_utils/block/crop/AbstractBaseCropBlock.java
+++ src/main/java/lemon_juice/scalar_utils/block/crop/AbstractBaseCropBlock.java
@@ -1,16 +1,17 @@
 package lemon_juice.scalar_utils.block.crop;
 
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
-public class AbstractBaseCrop extends CropBlock {
+public class AbstractBaseCropBlock extends CropBlock {
     private static final int minAge = 0;
     private static final int maxAge = 7;
     public static final IntegerProperty AGE = IntegerProperty.create("age", minAge, maxAge);
-    public AbstractBaseCrop(Properties properties) {
+    public AbstractBaseCropBlock(Properties properties) {
         super(properties);
     }
 
--- src/main/java/lemon_juice/scalar_utils/block/crop/FlaxCropBlock.java
+++ src/main/java/lemon_juice/scalar_utils/block/crop/FlaxCropBlock.java
@@ -2,14 +2,8 @@ package lemon_juice.scalar_utils.block.crop;
 
 import lemon_juice.scalar_utils.item.ModItems;
 import net.minecraft.world.level.ItemLike;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.CropBlock;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
-public class FlaxCropBlock extends AbstractBaseCrop {
+public class FlaxCropBlock extends AbstractBaseCropBlock {
 
     public FlaxCropBlock(Properties properties) {
         super(properties);
